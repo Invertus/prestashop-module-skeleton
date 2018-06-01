77b222a4988afc71b5e1d66dc53c26fffd9cfb18
Tomas Ilginis <tomas.ilginis@invertus.lt>
2018-06-01 17:10:47 +0300

    fixes autoload

--- skeleton.php
+++ skeleton.php
@@ -37,13 +37,13 @@ class Skeleton extends Module
         $this->tab = 'other_modules';
         $this->name = 'skeleton';
         $this->version = '1.0.0';
-        $this->displayName = $this->l('Skeleton');
-        $this->description = $this->l('This is module description');
         $this->author = 'Invertus';
 
         parent::__construct();
         $this->autoLoad();
         $this->compile();
+        $this->displayName = $this->l('Skeleton');
+        $this->description = $this->l('This is module description');
     }
 
     public function getTabs()
@@ -98,11 +98,7 @@ class Skeleton extends Module
      */
     private function autoLoad()
     {
-        $autoLoadPath = $this->getLocalPath().'/vendor/autoload.php';
-
-        if (!file_exists($autoLoadPath)) {
-            throw new FileNotFoundException('autoload.php file was not found on your module. Run composer install.');
-        }
+        $autoLoadPath = $this->getLocalPath().'vendor/autoload.php';
 
         require_once $autoLoadPath;
     }
